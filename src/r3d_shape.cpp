6e27086d03071e2606c5b8eb93aae7fec9d78690
Pocol <project.asura@gmail.com>
2017-09-05 12:33:45 +0900

    テクスチャ対応。

---
+++ src/r3d_shape.cpp
@@ -0,0 +1,48 @@
+﻿//-------------------------------------------------------------------------------------------------
+// File : r3d_shape.cpp
+// Desc : Shape.
+// Copyright(c) Project Asura. All right reserved.
+//-------------------------------------------------------------------------------------------------
+
+//-------------------------------------------------------------------------------------------------
+// Includes
+//-------------------------------------------------------------------------------------------------
+#include <r3d_shape.h>
+
+
+namespace {
+
+
+} // namespace
+
+#if 0
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Mesh class
+///////////////////////////////////////////////////////////////////////////////////////////////////
+Mesh* Mesh::create(const char* filename)
+{
+    auto instance = new(std::nothrow) Mesh();
+    if (!instance->load(filename))
+    {
+        delete instance;
+        return nullptr;
+    }
+
+    return instance;
+}
+
+bool Mesh::shadow_hit(const Ray& ray, float& dist) const
+{
+    return false;
+}
+
+bool Mesh::hit(const Ray& ray, HitRecord& record) const
+{
+    return false;
+}
+
+bool Mesh::load(const char* filename)
+{
+    return false;
+}
+#endif
